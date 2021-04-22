980d2053b422f486bf81d9e87222feb144a3bb2e
ppp.shiro46@gmail.com <ppp.shiro46@gmail.com>
2021-04-22 16:01:42 +0300

    work ref

---
+++ basic.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+ "fmt"
+)
+
+type Hoge struct{
+  x,y int
+}
+
+func main(){
+  v := Hoge{1,2}
+  
+}
--- http-server.go
+++ http-server.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"gopkg.in/gorp.v2"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    text := "hogehoge."
-    fmt.Fprint(w, "Hi there, I'm ", text)
+type DBManager struct {
+	DBMap *gorp.DbMap
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	text := "hogehoge."
+	fmt.Fprint(w, "Hi there, I'm ", text)
+}
+
+func CreateManager(user string, password string) *DBManager {
+	var source = user + ":" + password + "@tcp(localhost:3306)/discord_clone_database?parseTime=true"
+	var db, error = sql.Open("mysql", source)
+	if error != nil {
+		log.Fatal("error connecting to database:", error)
+	}
+	var gorpDialect = gorp.MySQLDialect{}
+	var dbMap = &gorp.DbMap{Db: db, Dialect: gorpDialect}
+	var manager = &DBManager{DBMap: dbMap}
+	manager.setup()
+
+	return manager
+}
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
